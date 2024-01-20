abb6d05b204c144c59d42e3f8e5aefc99b77f36c
rahul-evolvier <134580783+rahul-evolvier@users.noreply.github.com>
2024-01-20 15:15:56 +0530

    gsdfgs

---
+++ new.js
@@ -0,0 +1,25 @@
+const nnn = {
+    "@context": "https://schema.org/",
+    "@type": "Product",
+    "name": "OOTY MUSHROOM",
+    "productID": "sgdsgdjkhj",
+    "description": "we have fresh mushroom",
+    "aggregateRating": {
+        "@type": "AggregateRating",
+        "ratingValue": 5.0,
+        "reviewCount": 1
+    },
+    "offers": [{
+        "@type": "Offer",
+        "price": 300,
+        "priceCurrency": "INR",
+        "availability": "https://schema.org/InStock",
+        "condition": "new"
+    }],
+    "image": ["https://web.myneibo.com/cdn/storage/productImages/tHEQvsFKkA4PtfyGR/original/tHEQvsFKkA4PtfyGR.jpg?size=hdpi", "https://web.myneibo.com/cdn/storage/productImages/TvNQkQiWQzCemdWEp/original/TvNQkQiWQzCemdWEp.jpg?size=hdpi", "https://web.myneibo.com/cdn/storage/productImages/n5DNkBw3gbfdN9K6S/original/n5DNkBw3gbfdN9K6S.jpeg?size=hdpi"],
+    "brand": {
+        "@type": "Brand",
+        "name": "EAGLE THREADS & BITES"
+    },
+    "url": "https://www.myneibo.com/ooty-mushroom/eagle-threads-bites/pd/Ewx8FTESND8GYnYpD",
+}
